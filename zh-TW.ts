5650755024819d2ef45bf5fcf13f6850bd3bee4a
Elliot <elliotchen2010@gmail.com>
2024-10-29 22:42:54 +0800

    zh-tw update

---
+++ zh-TW.ts
@@ -0,0 +1,275 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwin.ui" line="20"/>
+        <source>RMxLRC - By ElliotCHEN37</source>
+        <translation>RMxLRC - 作者：ElliotCHEN37</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="54"/>
+        <source>Required parameters</source>
+        <translation>必填參數</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="75"/>
+        <source>Artist:</source>
+        <translation>藝術家：</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="82"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Artist of the song&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;歌曲的藝術家&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="98"/>
+        <source>Title:</source>
+        <translation>標題：</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="105"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Name of the song&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;歌曲的名稱&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="141"/>
+        <source>Optional parameters</source>
+        <translation>選用參數</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="156"/>
+        <source>Album:</source>
+        <translation>專輯：</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="163"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Album of the song&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;歌曲專輯&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="176"/>
+        <source>Sleep:</source>
+        <translation>睡眠：</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="183"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Sleep time in second during downloading multiple lyrics to avoid temporary ban from Musixmatch (Only effect Directory Mode)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;以秒來計算的睡眠時間，可防止被Musixmatch暫時性封禁（僅對批量模式有效）&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="186"/>
+        <source>30</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="203"/>
+        <source>Token:</source>
+        <translation>使用者令牌：</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="210"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Your Musixmatch user token for Windows (If you don&apos;t have one, RMxLRC will generate a new one for you)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;你的Windows平台的Musixmatch令牌（如果你沒有，RMxLRC會自動產生一個新的）&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="217"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Generate new user token&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;生成新的使用者令牌&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="240"/>
+        <source>Lyric Type:</source>
+        <translation>歌詞種類：</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="248"/>
+        <source>Synced</source>
+        <translation>已同步</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="253"/>
+        <source>Unsynced</source>
+        <translation>未同步</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="266"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Download lyric from Musixmatch&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;從Musixmatch下載歌詞&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="269"/>
+        <source>Download</source>
+        <translation>下載</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="286"/>
+        <source>File</source>
+        <translation>檔案</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="297"/>
+        <source>Edit</source>
+        <translation>編輯</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="303"/>
+        <source>Help</source>
+        <translation>幫助</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="319"/>
+        <source>Open audio file</source>
+        <translation>打開音訊檔</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="322"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="330"/>
+        <source>Read/Generate config file</source>
+        <translation>讀取/釋放配置檔</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="333"/>
+        <source>Ctrl+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="341"/>
+        <source>Exit</source>
+        <translation>離開</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="344"/>
+        <source>Alt+F4</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="352"/>
+        <source>Open config file</source>
+        <translation>打開配置檔</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="360"/>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="363"/>
+        <source>Alt+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="371"/>
+        <source>License</source>
+        <translation>授權</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="374"/>
+        <source>Alt+L</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="382"/>
+        <source>Release Page</source>
+        <translation>發佈頁</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="385"/>
+        <source>Alt+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="393"/>
+        <source>Open Directory</source>
+        <translation>批量模式</translation>
+    </message>
+    <message>
+        <location filename="mainwin.ui" line="396"/>
+        <source>Ctrl+D</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>self.app</name>
+    <message>
+        <location filename="uiproc.py" line="28"/>
+        <source>RMxLRC GUI {self.vernum} By ElliotCHEN37</source>
+        <translation>RMxLRC GUI {self.vernum} 作者：ElliotCHEN37</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="30"/>
+        <source>Ready</source>
+        <translation>準備完成</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="58"/>
+        <source>Select Directory</source>
+        <translation>選擇資料夾</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="65"/>
+        <source>Input Error</source>
+        <translation>輸入錯誤</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="66"/>
+        <source>Please enter a valid number for the time interval.</source>
+        <translation>請輸入有效的睡眠時間。</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="72"/>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="76"/>
+        <source>RMxLRC GUI {self.vernum} By ElliotCHEN37
+A program that can download lyrics you want from Musixmatch freely.
+Used projects:
+- PySide6
+- QFluentWidgets
+App icon is from Google&apos;s Material Symbols</source>
+        <translation>RMxLRC GUI {self.vernum} 作者：ElliotCHEN37
+一個用於從Musixmatch自由下載歌詞檔案的程式
+使用的項目：
+- PySide6
+- QFluentWidgets
+軟體圖標來自 Google 的 Material Symbols</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="79"/>
+        <source>License</source>
+        <translation>授權</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="126"/>
+        <source>Error</source>
+        <translation>錯誤</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="126"/>
+        <source>Failed to load config: {str(e)}</source>
+        <translation>加載配置檔失敗 {str(e)}</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="139"/>
+        <source>Token refreshed successfully.</source>
+        <translation>使用者令牌刷新成功。</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="141"/>
+        <source>Failed to refresh token.</source>
+        <translation>使用者令牌刷新失敗。</translation>
+    </message>
+    <message>
+        <location filename="uiproc.py" line="143"/>
+        <source>An error occurred: {str(e)}</source>
+        <translation>一個錯誤發生了 {str(e)}</translation>
+    </message>
+</context>
+</TS>
